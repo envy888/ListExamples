026655e6b04e88cbf40f3341c9b6b3baccc65662
envy888 <eg.goodhunt@gmail.com>
2022-08-04 16:15:19 +0300

    Возможности списков

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Example1
 {
@@ -6,7 +7,39 @@ namespace Example1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); // список это обобщенная коллекция пронумерованных элементов, список - динамическая структура данных, ее размер может меняться 
+            List<int> numbers = new List<int>(); // создание пустого списка 
+            numbers.Add(5);
+            Console.WriteLine(ListToString(numbers));
+            Console.WriteLine(ListToString(RandomList(10)));
+
+            List<int> testlist = RandomList(10);
+            Console.WriteLine(ListToString(testlist));
+            int a = testlist[2];
+            Console.WriteLine(a);
+        }
+
+        private static string ListToString(List<int> numbers)
+        {
+            string result = string.Empty;
+            foreach (int number in numbers)
+            {
+                result += number.ToString() + " ";
+            }
+
+            return result;
+        }
+        
+        private static List<int> RandomList(int length, int min = -1000, int max = 1000)
+        {
+            Random rng = new Random();
+            List<int> result = new List<int>(); 
+            for (int i = 0; i < length; i++)
+            {
+                result.Add(rng.Next(min, max));
+            }
+
+            return result;
         }
     }
 }
