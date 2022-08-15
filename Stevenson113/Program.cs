4b8103d9b78d2838e91f9afaf0026b49fd88dd67
envy888 <eg.goodhunt@gmail.com>
2022-08-15 12:47:20 +0300

    Вывод списка без дубликатов

---
+++ Stevenson113/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stevenson113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = EnterList();
+            Console.WriteLine(ListToString(words));
+            Console.WriteLine(ListToString(RemoveDuplicates(words)));
+            
+        }
+        
+        private static string ListToString(List<string> strings)
+        {
+            string result = string.Empty;
+            foreach (string str in strings)
+            {
+                result += str + " ";
+            }
+
+            return result;
+        }
+        
+        private static List<string> EnterList()
+        {
+            List<string> result = new List<string>();
+            
+            string enter = Console.ReadLine();
+            while (enter != string.Empty)
+            {
+                result.Add(enter);
+                enter = Console.ReadLine();
+            }
+            
+            return result;
+        }
+
+        private static List<string> RemoveDuplicates(List<string> words)
+        {
+            List<string> uniqueWords = new List<string>();
+            
+            foreach (string word in words)
+            {
+                if ( !uniqueWords.Contains(word))
+                {
+                    uniqueWords.Add(word);
+                }
+            }
+
+            return uniqueWords;
+        }
+        
+        
+        
+        
+        
+        
+        
+    }
+}
